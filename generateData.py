467e581a4fb4a0d2e2e1f4f7b2559905473755ae
Atharv Bhat <bhat.atharv@gmail.com>
2021-04-19 19:25:00 -0400

    generateData script for single image inputs

--- generateData.py
+++ generateData.py
@@ -15,13 +15,34 @@ import cv2
 import numpy as np
 import glob
 from tqdm import tqdm
+import time
 
 # read paths
 video_paths = glob.glob("Videos/*/*.*", recursive=True)
 
 #read videos
 
+#counter
+i = 0
+
 for video_path in tqdm(video_paths):
-    ret, capture = cv2.VideoCapture(video)
-    while ret :
-        
+    capture = cv2.VideoCapture(video_path)
+    while capture.isOpened() :
+        _ , frame = capture.read() # read frame
+
+        try:
+            cv2.imwrite(f"dataset/ground_truth/{i}.bmp", frame) # write ground truth
+            #delete alternate even and odd rows
+            if i%2 == 0 :
+                frame = del_even(frame) #delete even rows
+            else:
+                frame = del_odd(frame) #delete odd
+
+            cv2.imwrite(f"dataset/interlaced/{i}.bmp", frame) #write interlaced frame
+
+            i += 1
+
+        except:
+            break # this is stupid but it works
+
+    capture.release()
